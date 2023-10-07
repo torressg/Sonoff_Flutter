7b2632ef34a7101175836acfaeb37bbbb6fe031a
Guilherme Torres <guilhermevanderlei0210@gmail.com>
2023-10-07 17:29:30 -0300

    error loading & nice loading

--- on_off/lib/pages/buttons_page.dart
+++ on_off/lib/pages/buttons_page.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -17,6 +18,7 @@ class _ButtomPageState extends State<ButtomPage> {
   List<String> nameLight = [];
   int responseQtt = 0;
   List<String> statusSwitches = [];
+  bool errorLoad = false;
 
   // Call API to turn ON/OFF
   Future<void> changeSwitch(light) async {
@@ -40,22 +42,32 @@ class _ButtomPageState extends State<ButtomPage> {
 
   // Call API to get characteristics about Device
   Future<void> aboutDevice() async {
-    final responseName = await http
-        .get(Uri.parse('http://${dotenv.env['IP']}:7777/status/1000ba1e43'));
-    if (responseName.statusCode == 200) {
-      var decodeResponse = jsonDecode(responseName.body) as List;
-      setState(() {
-        // Getting name os switches
-        nameLight =
-            decodeResponse.map((item) => item['Nome'] as String).toList();
-        // Getting switches quantity
-        responseQtt = decodeResponse.length;
-        // Getting status switches
-        statusSwitches =
-            decodeResponse.map((item) => item['switch'] as String).toList();
-      });
-    } else {
-      print('Failed to load data. Status code: ${responseName.statusCode}');
+    try {
+      final responseName = await http
+          .get(Uri.parse('http://${dotenv.env['IP']}:7777/status/1000ba1e43'))
+          .timeout(Duration(seconds: 7));
+      if (responseName.statusCode == 200) {
+        var decodeResponse = jsonDecode(responseName.body) as List;
+        setState(() {
+          // Getting name os switches
+          nameLight =
+              decodeResponse.map((item) => item['Nome'] as String).toList();
+          // Getting switches quantity
+          responseQtt = decodeResponse.length;
+          // Getting status switches
+          statusSwitches =
+              decodeResponse.map((item) => item['switch'] as String).toList();
+        });
+      } else {
+        print('Failed to load data. Status code: ${responseName.statusCode}');
+      }
+    } on TimeoutException catch (e) {
+      errorLoad = true;
+      responseQtt = 0;
+      print('caiu');
+      setState(() {});
+    } catch (e) {
+      print('erro');
     }
   }
 
@@ -82,74 +94,111 @@ class _ButtomPageState extends State<ButtomPage> {
         child: Center(
           child: Container(
             margin: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-            child: responseQtt == 0
-                ? Center(
-                    child: CircularProgressIndicator(
-                      color: AppColors.primaryColor,
-                    ),
-                  )
-                : GridView.builder(
-                    gridDelegate:
-                        const SliverGridDelegateWithMaxCrossAxisExtent(
-                            maxCrossAxisExtent: 200,
-                            childAspectRatio: 3 / 2,
-                            crossAxisSpacing: 10,
-                            mainAxisSpacing: 10),
-                    itemCount: responseQtt,
-                    itemBuilder: (BuildContext ctx, index) {
-                      return ElevatedButton(
-                        style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all<Color>(
-                                AppColors.primaryColor)),
-                        onPressed: () async {
-                          await changeSwitch(
-                              (nameLight[index]).replaceAll(RegExp('á'), 'a'));
-                          setState(() {
-                            if (statusSwitches[index] == 'on') {
-                              statusSwitches[index] = 'off';
-                            } else if (statusSwitches[index] == 'off') {
-                              statusSwitches[index] = 'on';
-                            }
-                          });
-                        },
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Container(
-                              alignment: Alignment.center,
-                              child: statusSwitches[index] == 'on'
-                                  ? Icon(size: 50, Icons.lightbulb)
-                                  : Icon(size: 50, Icons.lightbulb_outlined),
-                            ),
-                            SizedBox(
-                              height: 5,
-                            ),
-                            Container(
+            child: errorLoad
+                ? Container(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Ocorreu um erro para se conectar.",
+                          style: TextStyle(
+                              color: AppColors.whiteColor,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20),
+                        ),
+                        SizedBox(height: 5),
+                        ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              errorLoad = false;
+                            });
+                            aboutDevice();
+                          },
+                          child: Container(
+                              height: 30,
+                              width: 120,
                               alignment: Alignment.center,
                               child: Text(
-                                'Luz ' + nameLight[index],
-                                style: TextStyle(fontSize: 15),
-                              ),
-                            ),
-                            SizedBox(
-                              height: 3,
-                            ),
-                            Container(
-                              alignment: Alignment.center,
-                              child: statusSwitches[index] == 'on'
-                                  ? Text(
-                                      'Ativada',
-                                      style: TextStyle(fontSize: 10),
-                                    )
-                                  : Text(
-                                      'Desativada',
-                                      style: TextStyle(fontSize: 10),
-                                    ),
-                            ),
-                          ],
+                                "Tentar novamente",
+                                style: TextStyle(fontSize: 14),
+                              )),
+                          style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                                  AppColors.primaryColor)),
+                        )
+                      ],
+                    ),
+                  )
+                : responseQtt == 0
+                    ? Center(
+                        child: CircularProgressIndicator(
+                          color: AppColors.primaryColor,
                         ),
-                      );
-                    }),
+                      )
+                    : GridView.builder(
+                        gridDelegate:
+                            const SliverGridDelegateWithMaxCrossAxisExtent(
+                                maxCrossAxisExtent: 200,
+                                childAspectRatio: 3 / 2,
+                                crossAxisSpacing: 10,
+                                mainAxisSpacing: 10),
+                        itemCount: responseQtt,
+                        itemBuilder: (BuildContext ctx, index) {
+                          return ElevatedButton(
+                            style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStateProperty.all<Color>(
+                                        AppColors.primaryColor)),
+                            onPressed: () async {
+                              await changeSwitch((nameLight[index])
+                                  .replaceAll(RegExp('á'), 'a'));
+                              setState(() {
+                                if (statusSwitches[index] == 'on') {
+                                  statusSwitches[index] = 'off';
+                                } else if (statusSwitches[index] == 'off') {
+                                  statusSwitches[index] = 'on';
+                                }
+                              });
+                            },
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  alignment: Alignment.center,
+                                  child: statusSwitches[index] == 'on'
+                                      ? Icon(size: 50, Icons.lightbulb)
+                                      : Icon(
+                                          size: 50, Icons.lightbulb_outlined),
+                                ),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                Container(
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                    'Luz ' + nameLight[index],
+                                    style: TextStyle(fontSize: 15),
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 3,
+                                ),
+                                Container(
+                                  alignment: Alignment.center,
+                                  child: statusSwitches[index] == 'on'
+                                      ? Text(
+                                          'Ativada',
+                                          style: TextStyle(fontSize: 10),
+                                        )
+                                      : Text(
+                                          'Desativada',
+                                          style: TextStyle(fontSize: 10),
+                                        ),
+                                ),
+                              ],
+                            ),
+                          );
+                        }),
           ),
         ),
       ),
